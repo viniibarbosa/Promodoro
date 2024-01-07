5c5659935bef4a18032ff6c15d214b9b516cbe1b
Vinicius <vinicius03barbosa@gmail.com>
2024-01-07 08:03:08 -0300

    configurado o cronometro

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -9,37 +9,43 @@ export class PomodoroComponent implements OnInit {
 
   botaoSelecionado:string = "pomodoro";
   tempoSelecionado:number = 0;
+  
   async executarCronometro(){
-    this.tempoSelecionado = (this.tempo as any)[this.botaoSelecionado];
+    this.tempoSelecionado = (this.tempo as any)[this.botaoSelecionado]['minutos'];
+    const minutos = this.tempoSelecionado
+    this.tempoSelecionado = this.tempoSelecionado*60*1000
     console.log(this.tempoSelecionado)
-
-    while (this.tempoSelecionado > 0) {
+    while (this.tempoSelecionado > 0 ) {
+      console.log(this.tempoSelecionado)
       await this.delay(1000, this.tempoSelecionado); // Aguarda 1 segundo
 
-      console.log(this.tempoSelecionado);
+      console.log((this.tempo as any)[this.botaoSelecionado]);
       this.tempoSelecionado = this.tempoSelecionado - 1000;
     }
+    (this.tempo as any)[this.botaoSelecionado]['segundosRestantes'] = '00';
+    (this.tempo as any)[this.botaoSelecionado]['minutos'] = minutos;
   }
 
   delay(ms: number, tempo:number) {
     (this.tempo as any)[this.botaoSelecionado] = this.converterSegundosParaMinutosESegundos(tempo/1000)
-    console.log((this.tempo as any)[this.botaoSelecionado])
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   tempo:any ={
-    "longBreak" : 10*60*1000,
-    "shortBreak": 5*60*1000,
-    "pomodoro": 1*60*1000
+    "longBreak" : {"minutos" :3, "segundosRestantes": "00"},
+    "shortBreak": {"minutos" :2, "segundosRestantes": "00"},
+    "pomodoro": {"minutos" :1, "segundosRestantes": "00"}
   }
   eventoSelecionado(eventSelecionado:string){
     this.botaoSelecionado = eventSelecionado
+    console.log(this.botaoSelecionado)
   }
+
+
 
   converterSegundosParaMinutosESegundos(segundos:number) {
     const minutos = Math.floor(segundos / 60);
-    const segundosRestantes = segundos % 60;
-
+    let segundosRestantes = (segundos % 60).toString().padStart(2, '0');
     return { minutos, segundosRestantes };
   }
 
